84b3118d630257df242a8b7815e81e4d76c0fb6e
heyzzin <112155730+heyzzin@users.noreply.github.com>
2022-10-14 10:59:13 +0900

    Update Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,28 +1,28 @@
 import './Timer.css';
 import { useState, useEffect } from "react";
 export default function Timer(){
-    const [data, setData] = useState(10);
+    const [second, setSecond] = useState(10);
     const [work, setWork] = useState(false);
     useEffect(() => {
         const timer = setInterval(()=>{
-            if(!work || data === 0) {
+            if(!work || second === 0) {
                 clearInterval(timer);
                 setWork(false);
-            }
-            else setData(data-1);
+            } else
+            setSecond(second-1);
         }, 1000);
         return () => {
             clearInterval(timer);
-        }
+        };
     });
     const Button = (props) => {
         return <button onClick={() => {
             setWork(props.value);
-            props.name !== '재개' && props.value && setData(10);
+            props.name === '시작' && setSecond(10);
         }}>{props.name}</button>
     }
     return <>
-        <h1>{data} 초</h1>
+        <h1>{second} 초</h1>
         {!work && <Button name='시작' value={true}/>}
         {work && <Button name='중지' value={false}/>}
         {!work && data !==10 && <Button name='재개' value={true}/>}
