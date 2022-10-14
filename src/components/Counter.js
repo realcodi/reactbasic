e1705f2f9a418315f9e9c5c36c7dc1d22598ee3c
heyzzin <112155730+heyzzin@users.noreply.github.com>
2022-10-14 10:50:39 +0900

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import { useState } from 'react';
 export default function Counter() {
-    let number = 0;
+    const [number, setNumber] = useState(0);
     function onClick() {
-        number = number + 1;
+        setNumber(number + 1);
         console.log(number);
     }
     return <>
